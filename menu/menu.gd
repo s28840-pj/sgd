7b03de87d0cbe3b5b2d47fccc02a3816ec91e1af
Mateusz Lisewski <mlisek44@gmail.com>
2025-06-13 09:18:38 +0200

    added main menu and submenus

---
+++ menu/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_player_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/level.tscn")
+
+func _on_shop_pressed() -> void:
+	get_tree().change_scene_to_file("res://shop_menu/shop_menu.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://options_menu/options_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit(0)
