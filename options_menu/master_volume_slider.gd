94e05a50ca179560859db937f6f6c4aa7fa9ae9e
Mateusz Lisewski <mlisek44@gmail.com>
2025-06-16 15:20:11 +0200

    added saving/loading audio setting to/from config file

--- options_menu/master_volume_slider.gd
+++ options_menu/master_volume_slider.gd
@@ -1,20 +1,40 @@
 extends HSlider
 
-@export
-var bus_name: String
+@onready var bus_index = AudioServer.get_bus_index("Master")
 
-var bus_index: int
+func get_volume() -> float:
+	return AudioServer.get_bus_volume_db(bus_index)
+
+func set_volume(volume:float):
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(volume)
+	)
 
 func _ready() -> void:
-	bus_index = AudioServer.get_bus_index("Master")
+	load_volume_settings()
 	value_changed.connect(_on_value_changed)
 	
 	value = db_to_linear(
-		AudioServer.get_bus_volume_db(bus_index)
+		get_volume()
 	)
 	
 func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(
-		bus_index,
-		linear_to_db(value)
-	)
+	set_volume(value)
+	save_volume_settings()
+
+func load_volume_settings():
+	var config = ConfigFile.new()
+	var err = config.load("user://audio_settings.cfg")
+	if err != OK:
+		set_volume(0.0)
+		return
+	
+	var volume = config.get_value("audio","master_volume",0.0)
+	set_volume(db_to_linear(volume))
+	
+func save_volume_settings():
+	var config = ConfigFile.new()
+	config.load("user://audio_settings.cfg")
+	config.set_value("audio","master_volume",get_volume())
+	config.save("user://audio_settings.cfg")
--- options_menu/music_muted.gd
+++ options_menu/music_muted.gd
@@ -1,14 +1,40 @@
 extends CheckButton
 
 @onready var button = $"."
+@onready var bus_index = AudioServer.get_bus_index("music")
 
-var bus_index = AudioServer.get_bus_index("music")
+func mute_audio(mute_it:bool):
+	AudioServer.set_bus_mute(bus_index, mute_it)
+
+func is_audio_muted() -> bool:
+	return AudioServer.is_bus_mute(bus_index)
+
+func toogle_mute():
+	mute_audio(!is_audio_muted())
 
 func _ready() -> void:
-	if AudioServer.is_bus_mute(bus_index):
+	load_mute_settings()
+	if is_audio_muted():
 		button.button_pressed = true
 	else:
 		button.button_pressed = false
 
 func _on_pressed() -> void:
-	AudioServer.set_bus_mute(bus_index, !AudioServer.is_bus_mute(bus_index))
+	toogle_mute()
+	save_mute_settings()
+
+func load_mute_settings():
+	var config = ConfigFile.new()
+	var err = config.load("user://audio_settings.cfg")
+	if err != OK:
+		mute_audio(false)
+		return
+	
+	var is_muted = config.get_value("audio","music_is_muted",false)
+	mute_audio(is_muted)
+	
+func save_mute_settings():
+	var config = ConfigFile.new()
+	config.load("user://audio_settings.cfg")
+	config.set_value("audio","music_is_muted",is_audio_muted())
+	config.save("user://audio_settings.cfg")
