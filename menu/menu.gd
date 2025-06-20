fdfd7d11e7f6f75d74efa0e237d9d9e84021c83b
Mateusz Lisewski <mlisek44@gmail.com>
2025-06-20 15:41:44 +0200

    fixed audio levels at start of the game

--- menu/menu.gd
+++ menu/menu.gd
@@ -11,6 +11,27 @@ func _ready() -> void:
 	GameManager.hide_canvas()
 	GameManager.load_user_settings()
 	
+	var config = ConfigFile.new()
+	var err = config.load("user://audio_settings.cfg")
+	var bus_index = AudioServer.get_bus_index("Master")
+	var muted = config.get_value("audio","master_is_muted",false)
+	var volume = config.get_value("audio","master_volume",0.0)
+	changeMutedVolume(bus_index, muted, db_to_linear(volume))
+	bus_index = AudioServer.get_bus_index("music")
+	muted = config.get_value("audio","music_is_muted",false)
+	volume = config.get_value("audio","music_volume",0.0)
+	changeMutedVolume(bus_index, muted, db_to_linear(volume))
+	bus_index = AudioServer.get_bus_index("sfx")
+	muted = config.get_value("audio","sfx_is_muted",false)
+	volume = config.get_value("audio","sfx_volume",0.0)
+	changeMutedVolume(bus_index, muted, db_to_linear(volume))
+	
+func changeMutedVolume(bus_index: int, muted: bool, volume: float):
+	AudioServer.set_bus_mute(bus_index,muted)
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(volume)
+	)
 
 func _on_play_pressed() -> void:
 	MenuButtonsSfx.play_button_click()
